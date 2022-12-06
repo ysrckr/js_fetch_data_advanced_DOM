6ad8af971e1232f4f65bc0d23ded8eff36cd1a09
ysrckr <dyt.yasarcakir@gmail.com>
2022-12-06 23:36:42 +0300

    request function added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const request = (url) => {
+  fetch(url).then((response) => {
+    if (!response.ok) {
+      return Promise.reject(
+        new Error(`${response.status} - ${response.statusText}`)
+      );
+    }
+
+    if (!response.headers.get('content-type').includes('application/json')) {
+      return Promise.reject(new Error('Response is not JSON'));
+    }
+
+    return response.json();
+  });
+};
